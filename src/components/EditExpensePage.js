62e4975e19f4b57786779f32514334d88464f851
Kamil Knap <66635413+Kamil14210@users.noreply.github.com>
2022-04-06 16:54:22 +0200

    Add all files to repository github

--- src/components/EditExpensePage.js
+++ src/components/EditExpensePage.js
@@ -1,11 +1,37 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { editExpense } from '../actions/expenses';
+import ExpenseForm from './ExpenseForm';
+import { removeExpense } from '../actions/expenses';
+
+
 const EditExpensePage =(props) =>{
-    console.log(props)
+    //console.log(props)
     return(
         <div>
-        This is Edit Expensepage.{props.match.params.id}
+        <ExpenseForm
+            expense={props.expense}
+            onSubmit={(expense)=>{
+                //console.log('updated', expense)
+                //edycja wpisu poprzez dispatch i powrót na str glowna
+                props.dispatch(editExpense(props.expense.id, expense));
+                props.history.push('/')
+            }}
+        />
+        <button 
+        //usuwanie wpisu poprzez dispatch
+        onClick={()=>{
+            props.dispatch(removeExpense({id: props.expense.id} ));
+            props.history.push('/')
+        }}>Remove</button>
     </div>
     )
 }
 
-export default EditExpensePage;
+const mapStateToProps = (state, props) =>{
+    return{
+        expense: state.expenses.find((expense)=> expense.id === props.match.params.id)
+    }
+}
+
+export default connect(mapStateToProps)(EditExpensePage) ;
--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -8,14 +8,17 @@ import 'react-dates/lib/css/_datepicker.css';
 //console.log(now.format('MMM Do, YYYY'));
 
 export default class ExpenseForm extends React.Component{
-    state = {
-        description: '',
-        amount: '',
-        note: '',
-        createdAt: moment(),
-        calendarFocused: false,
-        error: ''
-    };
+    constructor(props){
+        super(props)
+        this.state = {
+            description: props.expense ? props.expense.description : '',
+            amount: props.expense ? (props.expense.amount /100).toString() : '',
+            note: props.expense ? props.expense.note : '',
+            createdAt: props.expense ? moment(props.expense.createdAt) : moment(),
+            calendarFocused: false,
+            error: ''
+        }
+    }
     onDescriptionChange = (e) =>{
         const description = e.target.value;
         const note = e.target.value;
